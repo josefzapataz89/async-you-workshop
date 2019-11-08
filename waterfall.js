76b7287ca4c38312610a5d62a1034a3c4ac04f77
Jose Fernando Zapata Zambrano <joferzz1989@gmail.com>
2019-11-08 16:47:41 -0400

    :jack_o_lantern: feat: async

---
+++ waterfall.js
@@ -0,0 +1,33 @@
+
+var fs = require('fs')
+var http = require('http')
+var async = require('async');
+
+async.waterfall([
+    function(done){
+        fs.readFile(process.argv[2], function(err, data){
+            if (err) return done(err);
+            done(null, data)
+        });
+    },
+
+    function(data, done){
+        var body = '';
+        http.get(data.toString().trimRight(), function(res){
+            res.on('data', function(chunk){
+                body += chunk.toString();
+            });
+
+            res.on('end', function(chunk){
+                 done(null, body);
+            });
+        }).on('error', function(e){
+            done(e);
+        });
+    }
+], function done(err, result){
+    if (err) 
+        return console.error(err);
+        
+    console.log(result);
+});
